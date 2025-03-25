936f4c0115b3dec7a1eaa51e9130df62169cf14b
Maruf Hossain Rafi <2222222@iub.edu.bd>
2025-03-25 10:35:46 +0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.project_covid_app {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.project_covid_app to javafx.fxml;
+    exports com.example.project_covid_app;
+}
