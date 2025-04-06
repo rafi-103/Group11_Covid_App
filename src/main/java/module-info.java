5a67924ff7e9b42a14277ede5f0244b2dba1e5d1
cis101 <rafi2039103@gmail.com>
2025-04-06 12:59:09 +0600

    Rafi  :- FXML and Controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module com.example.project_covid_app {
 
     opens com.example.project_covid_app to javafx.fxml;
     exports com.example.project_covid_app;
+
+    opens com.example.project_covid_app.Rafi to javafx.fxml;
+    exports com.example.project_covid_app.Rafi;
 }
