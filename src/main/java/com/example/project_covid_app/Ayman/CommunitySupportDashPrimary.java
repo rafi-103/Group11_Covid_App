2048d432d14f7edd3dcfd3d3a3601a9e46e459d9
Ayman <aymanrahman30@gmail.com>
2025-04-15 05:33:44 +0600

    Ayman PatientDashPrimary,CommunityDashPrimary,PatientFXML,CommunityFXML

---
+++ src/main/java/com/example/project_covid_app/Ayman/CommunitySupportDashPrimary.java
@@ -0,0 +1,7 @@
+package com.example.project_covid_app.Ayman;
+
+public class CommunitySupportDashPrimary
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/example/project_covid_app/Ayman/PatientDashPrimary.java
@@ -0,0 +1,7 @@
+package com.example.project_covid_app.Ayman;
+
+public class PatientDashPrimary
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
