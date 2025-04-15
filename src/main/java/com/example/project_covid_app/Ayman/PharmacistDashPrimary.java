12de881a59894833267d7a43164b83ef96dd2c4d
Ayman <aymanrahman30@gmail.com>
2025-04-15 06:08:57 +0600

    Ayman PharmacistDashFXML

--- src/main/java/com/example/project_covid_app/Ayman/PharmacistDashPrimary.java
+++ src/main/java/com/example/project_covid_app/Ayman/PharmacistDashPrimary.java
@@ -3,5 +3,14 @@ package com.example.project_covid_app.Ayman;
 public class PharmacistDashPrimary
 {
     @javafx.fxml.FXML
+    private Label pharmacistDashboardTitleLabel;
+    @javafx.fxml.FXML
+    private HBox hBoxForNagivation;
+    @javafx.fxml.FXML
+    private Button backButtonPharmacistDashHbox;
+    @javafx.fxml.FXML
+    private Button homeButtonPharmacistDashHbox;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
